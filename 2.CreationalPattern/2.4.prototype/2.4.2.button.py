e8fce33d031baf6d42b5bb89f440db9b13d0ab37
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-09-27 18:28:02 +0200

    0003: singleton

--- 2.CreationalPattern/2.4.prototype/2.4.2.button.py
+++ 2.CreationalPattern/2.4.prototype/2.4.2.button.py
@@ -1,9 +1,25 @@
-# import copy
-#
-# class Button:
-#     # your code here
-#
-# win_checkbox = # your code here
-# win_pushbutton = # your code here
-# mac_checkbox = # your code here
-# mac_pushbutton = # your code here
+import copy
+
+
+class Button:
+    def __init__(self, os, button_type):
+        if os == 'Windows':
+            self.colour = 'синий'
+        else:
+            self.colour = 'белый'
+
+        self.button_type = button_type
+
+        if self.button_type == 'Чекбокс':
+            self.message = f'Галочка поставлена, цвет {self.colour}'
+        else:
+            self.message = f'На кнопку нажали, цвет {self.colour}'
+
+    def push(self):
+        print(self.message)
+
+
+win_checkbox = Button('Windows', 'Чекбокс')
+win_pushbutton = Button('Windows', 'Кнопка Нажатия')
+mac_checkbox = Button('Mac', 'Чекбокс')
+mac_pushbutton = Button('Mac', 'Кнопка Нажатия')
---
+++ 2.CreationalPattern/2.5.singleton/2.5.1.Singleton.py
@@ -0,0 +1,22 @@
+class Singleton(type):
+
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__call__(*args, **kwargs)
+        return cls._instance
+
+
+class MyClass(metaclass=Singleton):
+    pass
+
+
+def main():
+    m1 = MyClass()
+    m2 = MyClass()
+    assert m1 is m2
+
+
+if __name__ == "__main__":
+    main()
